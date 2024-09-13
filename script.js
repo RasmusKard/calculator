d18c523cdeaa87a0a132693652923ee4057798a6
RasmusKard <rasmuskard@gmail.com>
2024-09-13 16:10:38 +0300

    add: html, css, finish calculator and display logic

--- script.js
+++ script.js
@@ -18,3 +18,55 @@ function operate(numA, operator, numB) {
 			return divide(numA, numB);
 	}
 }
+
+let firstNum = "";
+let operator = "";
+let secondNum = "";
+const operators = ["+", "-", "*", "/"];
+
+// button eventlistener
+const calculatorButtonContainer = document.querySelector(
+	"#calc-button-container"
+);
+
+calculatorButtonContainer.addEventListener("click", (e) => {
+	let buttonPressed = e.target.innerHTML;
+
+	if (!isNaN(buttonPressed)) {
+		numDisplay(buttonPressed);
+	} else if (operators.includes(buttonPressed)) {
+		if (operator) {
+			firstNum = operate(firstNum, operator, secondNum);
+			operator = buttonPressed;
+			secondNum = "";
+		} else {
+			operator = buttonPressed;
+		}
+	} else if (buttonPressed === "=") {
+		firstNum = operate(firstNum, operator, secondNum);
+		operator = "";
+		secondNum = "";
+	} else if (buttonPressed === "C") {
+		firstNum = "";
+		secondNum = "";
+		operator = "";
+	}
+
+	document.querySelector(
+		"#calc-display"
+	).innerText = `${firstNum} ${operator} ${secondNum}`;
+});
+
+function numDisplay(num) {
+	if (operator) {
+		secondNum += num;
+	} else {
+		firstNum += num;
+	}
+}
+
+// if is number add to display of calculation (concat numbers as strings)
+// if is not number switch case
+// if it is an operator check save and display the operator and start listening for second number
+// if it is "C" clear the display and values
+// if it is "=" try to do the calculation
